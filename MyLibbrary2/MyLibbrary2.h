2a23811e5434a2d30cd8a2fe18f0a122ade32106
Sazid Iqbal <sazid.iqbal@mobcoder.com>
2022-06-09 12:44:37 +0530

    Initial Commit

---
+++ MyLibbrary2/MyLibbrary2.h
@@ -0,0 +1,18 @@
+//
+//  MyLibbrary2.h
+//  MyLibbrary2
+//
+//  Created by Mobcoder Technologies Private Limited on 09/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLibbrary2.
+FOUNDATION_EXPORT double MyLibbrary2VersionNumber;
+
+//! Project version string for MyLibbrary2.
+FOUNDATION_EXPORT const unsigned char MyLibbrary2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLibbrary2/PublicHeader.h>
+
+
